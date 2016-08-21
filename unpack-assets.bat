6293ecdefc822fd8caecd4ed6a0a40940bab5458
HyperHamster <officialhyperhamster@gmail.com>
2016-08-21 07:58:08 -0500

    Inital commit

---
+++ unpack-assets.bat
@@ -0,0 +1,115 @@
+@echo off
+
+if "%*"=="/?" goto :help
+if "%*"=="" set mode=:unpack_starbound&& goto :choice
+if "%*"=="/a" set mode=:unpack_workshop_all&& goto :choice
+if "%*"=="/A" set mode=:unpack_workshop_all&& goto :choice
+set mode=:unpack_workshop
+
+:choice
+
+echo 1: Select default 64-bit system Steam Library location (C:\Program Files (x86)\Steam).
+echo 2: Select default 32-bit system Steam Library location (C:\Program Files\Steam).
+echo 3: Enter your own custom Steam Library location.
+echo K: Select Kirby^'s Steam Library location (D:\Windows\Steam Games).
+echo Q: Quit.
+
+choice /c:Q123K /n
+
+if "%errorlevel%"=="1" goto :end
+if "%errorlevel%"=="2" set steam_library=C:\Program Files ^(x86^)\Steam&& goto :begin
+if "%errorlevel%"=="3" set steam_library=C:\Program Files\Steam&& goto :begin
+if "%errorlevel%"=="4" goto :prompt
+if "%errorlevel%"=="5" set steam_library=D:\Windows\Steam Games&& goto :begin
+
+:prompt
+
+set /p user_input=Specify your Steam Library location: 
+set steam_library=%user_input%
+
+:begin
+
+set starbound=%steam_library%\steamapps\common\Starbound
+set starbound_workshop=%steam_library%\steamapps\workshop\content\211820
+set unpack=%starbound%\win32\asset_unpacker.exe
+set starbound_assets=%starbound%\assets\packed.pak
+
+goto %mode%
+
+:unpack_starbound
+
+if exist "%starbound_assets%" (
+    if exist "%starbound%\_UnpackedAssets" (
+        echo Removing Starbound^'s old unpacked assets...
+        rd /s /q "%starbound%\_UnpackedAssets"
+        echo Done.
+    )
+    
+    echo Unpacking Starbound^'s assets...
+    "%unpack%" "%starbound_assets%" "%starbound%\_UnpackedAssets" >nul
+    echo Done.
+    goto :end
+)
+
+echo Starbound^'s assets not found.
+pause
+goto :end
+
+:unpack_workshop
+
+if exist "%starbound_workshop%\%1" (
+    if exist "%starbound%\mods\%1" (
+        echo Removing %1^'s old unpacked assets...
+        rd /s /q "%starbound%\mods\%1"
+        echo Done.
+    )
+    
+    echo Unpacking %1^'s assets...
+    "%unpack%" "%starbound_workshop%\%1\contents.pak" "%starbound%\mods\%1" >nul
+    echo Done.
+    goto :end
+)
+
+echo %1^'s assets not found.
+pause
+goto :end
+
+:unpack_workshop_all
+
+set num_loops=0
+for /f %%g in ('dir /b /a:d "%starbound_workshop%"') do (
+    set /a "num_loops+=1"
+    
+    if exist "%starbound%\mods\%%g" (
+        echo Removing %%g^'s old unpacked assets...
+        rd /s /q "%starbound%\mods\%%g"
+        echo Done.
+    )
+    
+    echo Unpacking %%g^'s assets...
+    "%unpack%" "%starbound_workshop%\%%g\contents.pak" "%starbound%\mods\%%g" >nul
+    echo Done.
+)
+
+if "%num_loops%"=="0" (
+    echo No mods found.
+    pause
+    goto :end
+)
+
+echo Finished unpacking %num_loops% mod(s).
+goto :end
+
+:help
+
+echo Unpacks Starbound assets.
+echo.
+echo unpack-assets.bat [/a] [workshop_id]
+echo.
+echo   /a    Unpack *ALL* Starbound Steam Workshop assets.
+echo.
+echo Given no arguments, unpacks Starbound's assets.
+echo Given one argument that is an installed Starbound Steam Workshop ID, unpacks its assets.
+echo Given the /a switch, unpacks *ALL* Starbound Steam Workshop assets.
+
+:end
---
+++ unpack-assets.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+if [[ ! -d "$HOME/.local/share/Scunt" ]]; then
+    echo "Default Steam Library directory not present on your system."
+    echo "Searching for Steam Library directories within your home directory..."
+    
+    if [[ -e /tmp/unpack-assets.tmp ]]; then rm '/tmp/unpack-assets.tmp'; fi
+    
+    while IFS= read -r -d ''; do
+        proper_dir=$(sed s_/steamapps.*__ < <(echo "$REPLY"))
+        
+        echo "$proper_dir" >> /tmp/unpack-assets.tmp
+    done < <(find $HOME -type d -path '*/steamapps' -print0 2>/dev/null)
+    
+    if [[ ! -e /tmp/unpack-assets.tmp ]]; then
+        echo "No Steam Libraries exist within your home directory."
+        exit 1
+    fi
+    
+    num_finds="$(wc -l < /tmp/unpack-assets.tmp)"
+    
+    echo "Choose the appropriate Steam Library directory by pressing the corresponding key."
+    cat -n '/tmp/unpack-assets.tmp' | sed -e 's/     //' -e 's/	/: /'
+    
+    while read -r -n 1 -s choice; do
+        if [[ $choice =~ [[:digit:]] && $choice -gt 0 && $choice -le $num_finds ]]; then
+            steam_library="$(sed "${choice}q;d" /tmp/unpack-assets.tmp)"
+            break
+        fi
+        
+        echo "Invalid choice, try again."
+    done
+    
+    if [[ -e /tmp/unpack-assets.tmp ]]; then rm '/tmp/unpack-assets.tmp'; fi
+else
+    steam_library="$HOME/.local/share/Steam"
+fi
+
+while getopts ":haHA" opt; do
+    case $opt in
+        h|H) hflag=1;;
+        a|A) aflag=1;;
+    esac
+done
+
+starbound="$steam_library/steamapps/common/Starbound"
+starbound_workshop="$steam_library/steamapps/workshop/content/211820"
+unpack="$starbound/linux/asset_unpacker"
+starbound_assets="$starbound/assets/packed.pak"
+
+unpack_starbound() {
+    if [[ -e $starbound_assets ]]; then
+        if [[ -d $starbound/_UnpackedAssets ]]; then
+            echo "Removing Starbound's old unpacked assets..."
+            rm -r $starbound/_UnpackedAssets
+            echo "Done."
+        fi
+        
+        echo "Unpacking Starbound's assets..."
+        $unpack $starbound_assets $starbound/_UnpackedAssets >/dev/null 2>&1
+        echo "Done."
+        exit 0
+    fi
+    
+    echo "Starbound's assets not found."
+    exit 1
+}
+
+unpack_workshop() {
+    if [[ -d $starbound_workshop/$1 ]]; then
+        if [[ -d $starbound/mods/$1 ]]; then
+            echo "Removing $1's old unpacked assets..."
+            rm -r $starbound/mods/$1
+            echo "Done."
+        fi
+        
+        echo "Unpacking $1's assets..."
+        $unpack $starbound_workshop/$1/contents.pak $starbound/mods/$1 >/dev/null 2>&1
+        echo "Done."
+    else
+        echo "$1's assets not found."
+        exit 1
+    fi
+}
+
+unpack_workshop_all() {
+    num_mods=0
+    for id in $(ls -1 $starbound_workshop); do
+        ((num_mods++))
+        
+        unpack_workshop $id
+    done
+    
+    if [[ $num_mods -eq 0 ]]; then
+        echo "No mods found."
+        echo "Please install some workshop mods before trying to unpack them."
+        exit 1
+    fi
+    
+    echo "Finished unpacking $num_mods mod(s)."
+    exit 0
+}
+
+print_help() {
+    echo "Usage: unpack-assets.sh [options] [workshop_id]"
+    echo
+    echo "Unpacks Starbound assets."
+    echo
+    echo "Options:"
+    echo " -h, --help  Print this help and exit."
+    echo " -a, --all   Unpack *ALL* Starbound Steam Workshop assets."
+    echo
+    echo "Given no arguments, unpacks Starbound's assets."
+    echo "Given one argument that is an installed Starbound Steam Workshop ID, unpacks its assets."
+    echo "Given the -a option, unpacks *ALL* Starbound Steam Workshop assets."
+    exit 0
+}
+
+if [[ $hflag -eq 1 ]]; then print_help; fi
+if [[ $# -eq 0 ]]; then unpack_starbound; fi
+if [[ $aflag -eq 1 ]]; then unpack_workshop_all; fi
+unpack_workshop $1
+exit 0
